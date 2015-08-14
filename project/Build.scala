98dd9bb312f28591c205d8e42c8ed78205d9e733
Stephen McDonald <steve@jupo.org>
2015-08-14 19:40:03 +1000

    Build file cleanup.

--- project/Build.scala
+++ project/Build.scala
@@ -5,9 +5,20 @@ import sbtassembly.AssemblyKeys._
 
 object CurioDB extends Build {
 
-  val scalaV = "2.11.7"
-  val akkaV  = "2.4-SNAPSHOT"
-  val sprayV = "1.3.3"
+  val scalaV   = "2.11.7"
+  val akkaV    = "2.4-SNAPSHOT"
+  val sprayV   = "1.3.3"
+  val curiodbV = "0.0.1"
+
+  val dependencies = Seq(
+    "com.typesafe.akka"   %% "akka-actor" % akkaV,
+    "com.typesafe.akka"   %% "akka-persistence-experimental" % akkaV,
+    "com.typesafe.akka"   %% "akka-cluster" % akkaV,
+    "io.spray"            %% "spray-can" % sprayV,
+    "io.spray"            %% "spray-json" % "1.3.2",
+    "com.wandoulabs.akka" %% "spray-websocket" % "0.1.4",
+    "org.luaj"             % "luaj-jse" % "3.0.1"
+  )
 
   val reverseConcat: MergeStrategy = new MergeStrategy {
     val name = "reverseConcat"
@@ -16,39 +27,24 @@ object CurioDB extends Build {
   }
 
   lazy val indexedTreeMap = RootProject(uri("git://github.com/stephenmcd/indexed-tree-map.git"))
-  lazy val hyperLogLog = RootProject(uri("git://github.com/stephenmcd/java-hll.git#with-fastutils"))
+  lazy val hyperLogLog    = RootProject(uri("git://github.com/stephenmcd/java-hll.git#with-fastutils"))
 
-  lazy val root = Project("root", file("."))
-    .dependsOn(indexedTreeMap, hyperLogLog)
-    .settings(
-      scalaVersion := scalaV,
-      scalaVersion in indexedTreeMap := scalaV,
-      scalaVersion in hyperLogLog := scalaV,
-      name := "curiodb",
-      version := "0.0.1",
-      fork := true,
-      mainClass in (Compile, run) := Some("curiodb.CurioDB"),
-      //resolvers += "Local Maven Repository" at "file://" + Path.userHome.absolutePath + "/.ivy2/local",
-      resolvers += "Akka Snapshots" at "http://repo.akka.io/snapshots/",
-      resolvers += "spray repo" at "http://repo.spray.io",
-      libraryDependencies ++= Seq(
-        "com.typesafe.akka" %% "akka-actor" % akkaV,
-        "com.typesafe.akka" %% "akka-persistence-experimental" % akkaV,
-        "com.typesafe.akka" %% "akka-cluster" % akkaV,
-        "io.spray" %% "spray-can" % sprayV,
-        "io.spray" %% "spray-json" % "1.3.2",
-        "com.wandoulabs.akka" %% "spray-websocket" % "0.1.4",
-        "org.luaj" % "luaj-jse" % "3.0.1"
-      ),
-      assemblyOption in assembly := (assemblyOption in assembly).value.copy(prependShellScript =
-        Option(Seq("#!/usr/bin/env sh", """ exec java -jar "$0" "$@" """))),
-      assemblyJarName in assembly := s"${name.value}-${version.value}",
-      assemblyMergeStrategy in assembly := {
-        case PathList(ps @ _*) if ps.takeRight(2).mkString("/") == "leveldb/DB.class" => MergeStrategy.first
-        case x if Assembly.isConfigFile(x) => reverseConcat
-        case x => (assemblyMergeStrategy in assembly).value(x)
-      }
-
-    )
+  lazy val root = Project("root", file(".")).dependsOn(indexedTreeMap, hyperLogLog).settings(
+    name := "curiodb",
+    version := curiodbV,
+    scalaVersion in Global := scalaV,
+    //resolvers += "Local Maven Repository" at "file://" + Path.userHome.absolutePath + "/.ivy2/local",
+    resolvers += "Akka Snapshots" at "http://repo.akka.io/snapshots/",
+    resolvers += "spray repo" at "http://repo.spray.io",
+    libraryDependencies ++= dependencies,
+    assemblyOption in assembly := (assemblyOption in assembly).value.copy(prependShellScript =
+      Option(Seq("#!/usr/bin/env sh", """exec java -jar "$0" "$@" """))),
+    assemblyJarName in assembly := s"${name.value}-${version.value}",
+    assemblyMergeStrategy in assembly := {
+      case PathList(ps @ _*) if ps.takeRight(2).mkString("/") == "leveldb/DB.class" => MergeStrategy.first
+      case x if Assembly.isConfigFile(x) => reverseConcat
+      case x => (assemblyMergeStrategy in assembly).value(x)
+    }
+  )
 
 }
