70de975d01759987d303ff73393e26b9e7ecde3b
meteorgan <meteorite.gan@gmail.com>
2015-12-10 21:49:10 +0800

    fix set aggregation operations when some key does not exist

--- src/main/scala/Commands.scala
+++ src/main/scala/Commands.scala
@@ -104,6 +104,7 @@ object Commands {
       case "neg1"   => -1
       case "neg2"   => -2
       case "seq"    => Seq()
+      case "set"    => mutable.Set()
       case "SCAN"   => Seq("0", "")
       case "nils"   => args.map(_ => null)
       case "zeros"  => args.map(_ => 0)
